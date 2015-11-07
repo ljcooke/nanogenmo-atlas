647052c716274dfae5ab4ddff59a8c6ca8f5787b
Liam Cooke <git@araile.com>
2015-11-07 02:26:27 +0000

    Move story-specific code out of corpora module

--- atlas/corpora.py
+++ atlas/corpora.py
@@ -1,8 +1,5 @@
 import json
 import os
-from glob import glob
-
-from .util import titlecase
 
 
 DATA_DIR = 'data/corpora/data'
@@ -17,23 +14,3 @@ def load(filename, key=None):
     vocab = load_json(filename)
     key = key or os.path.basename(filename)
     return vocab[key]
-
-
-def planet_names():
-    names = []
-
-    names += load('mythology/greek_gods')
-    names += load('mythology/greek_titans')
-    names += load('plants/flowers')
-
-    names += [spell['incantation'] for spell in load('words/spells')]
-
-    many = load('mythology/norse_gods', key='norse_deities')
-    names += many['gods'] + many['goddesses']
-
-    return set(filter(bool,
-                      (titlecase(name.split()[0]) for name in names)))
-
-
-if __name__ == '__main__':
-    print(planet_names())
--- atlas/story.py
+++ atlas/story.py
@@ -1,9 +1,27 @@
 from . import corpora
 from . import oec
 from .types import RandomStack
+from .util import titlecase
 
 
 DEFAULT_NUM_CHAPTERS = 50
+
+
+def planet_names():
+    names = []
+
+    names += corpora.load('mythology/greek_gods')
+    names += corpora.load('mythology/greek_titans')
+    names += corpora.load('plants/flowers')
+
+    names += [spell['incantation']
+              for spell in corpora.load('words/spells')]
+
+    many = corpora.load('mythology/norse_gods', key='norse_deities')
+    names += many['gods'] + many['goddesses']
+
+    return set(filter(bool,
+                      (titlecase(name.split()[0]) for name in names)))
 
 
 class Chapter:
@@ -20,15 +38,15 @@ class Story:
     def __init__(self, num_chapters=DEFAULT_NUM_CHAPTERS):
         self.exoplanets = oec.Exoplanets()
 
-        planet_names = RandomStack(corpora.planet_names())
-
         systems = self.exoplanets.stack()
+        names = RandomStack(planet_names())
+
         chapters = []
         for i in range(1, num_chapters + 1):
             system = systems.pop()
             star = system.random_star()
             planet = star.random_planet()
-            name = planet_names.pop()
+            name = names.pop()
             chapters.append(Chapter(i, planet, name))
         self.chapters = chapters
 
